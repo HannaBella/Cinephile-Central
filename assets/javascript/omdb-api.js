44afc32cf47263b263ebdf096bd77ef7cb6dfb01
Mitu Fikir <hannatefera2@gmail.com>
2019-12-13 21:53:11 -0800

    Updated omdb API key

--- assets/javascript/omdb-api.js
+++ assets/javascript/omdb-api.js
@@ -1,8 +1,7 @@
 var omdbId = "";
-var omdbAPI; //INSERT OMDB API
+
 function findMovieId(str) {
-    //var movie = $(this).attr("data-name");
-    var queryURL = "https://www.omdbapi.com/?t=" + str + "&apikey=" + omdbAPI;
+    var queryURL = "https://www.omdbapi.com/?t=" + str + "&apikey=trilogy";
     $.ajax({
         url: queryURL,
         method: "GET"
